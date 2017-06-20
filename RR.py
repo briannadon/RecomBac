d94e31987c6b71ce8e013914de648d446fdd110b
briannadon <brian.nadon@gmail.com>
2017-06-20 16:23:15 -0400

    Adding primer3 framework
    This update adds in the primer3 library for use in primer design, as there was little use in reinventing the wheel here.  This will add a dependency, but should be easy to install for linux/unix users.

--- RR.py
+++ RR.py
@@ -4,13 +4,40 @@ from Bio import SeqIO,pairwise2
 import re
 from sys import argv
 import argparse
+import primer3
 
 script, gb, left, right = argv
 left, right = int(left), int(right)
 #parser = argparse.ArgumentParser(description="Placeholder. Program to define primers in bacterial recombination.")
 
-region = SeqIO.read(gb,'genbank')
+seqrec = SeqIO.read(gb,'genbank')
+seqrec = str(seqrec.seq)
+seqincludedregion = [left,right]
 
-cutseq = str(region.seq)[(left-1):(right+1)]
-
-print(cutseq)
+primer3.bindings.designPrimers(
+    {
+        'SEQUENCE_ID': 'MH1000',
+        'SEQUENCE_TEMPLATE': seqrec,
+        'SEQUENCE_INCLUDED_REGION': seqincludedregion
+    },
+    {
+        'PRIMER_OPT_SIZE': 20,
+        'PRIMER_TASK': 'pick_cloning_primers',
+        'PRIMER_INTERNAL_MAX_SELF_END': 8,
+        'PRIMER_MIN_SIZE': 18,
+        'PRIMER_MAX_SIZE': 25,
+        'PRIMER_OPT_TM': 60.0,
+        'PRIMER_MIN_TM': 57.0,
+        'PRIMER_MAX_TM': 63.0,
+        'PRIMER_MIN_GC': 20.0,
+        'PRIMER_MAX_GC': 80.0,
+        'PRIMER_MAX_POLY_X': 100,
+        'PRIMER_INTERNAL_MAX_POLY_X': 100,
+        'PRIMER_SALT_MONOVALENT': 50.0,
+        'PRIMER_DNA_CONC': 50.0,
+        'PRIMER_MAX_NS_ACCEPTED': 0,
+        'PRIMER_MAX_SELF_ANY': 12,
+        'PRIMER_MAX_SELF_END': 8,
+        'PRIMER_PAIR_MAX_COMPL_ANY': 12,
+        'PRIMER_PAIR_MAX_COMPL_END': 8,
+    })
