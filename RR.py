d9eea0783d05218a15be7d9e99dff8f2bb863874
briannadon <brian.nadon@gmail.com>
2017-07-26 11:09:15 -0400

    Adding print methods

--- RR.py
+++ RR.py
@@ -20,9 +20,22 @@ args = parser.parse_args()
 seqrec = SeqIO.read(args.sequence,args.filetype)
 seqrec = str(seqrec.seq)
 
+def print_primers(lprimer, rprimer):
+     for k in sorted(lprimer.keys()):
+          print('%s\t%s' % (k,lprimer[k]))
+     for k in sorted(rprimer.keys()):
+          print('%s\t%s' % (k,rprimer[k]))
+          
+def write_primers(lprimer, rprimer, wfile):
+     for k in sorted(lprimer.keys()):
+          args.output.write('%s\t%s\n' % (k,lprimer[k]))
+     for k in sorted(rprimer.keys()):
+          args.output.write('%s\t%s\n' % (k,rprimer[k]))
+     args.output.write('\n\n')
+     
 
 #designing primers that bind the left flank
-leftflank = primer3.bindings.designPrimers(
+lflanklprimer = primer3.bindings.designPrimers(
     {
         'SEQUENCE_ID': 'Left Flank',
         'SEQUENCE_TEMPLATE': seqrec,
@@ -30,10 +43,12 @@ leftflank = primer3.bindings.designPrimers(
     },
     {
         'PRIMER_OPT_SIZE': 20,
-        'PRIMER_TASK': 'pick_cloning_primers',
+        'PRIMER_TASK': 'generic',
+        'PRIMER_PICK_LEFT_PRIMER': 1,
+        'PRIMER_PICK_RIGHT_PRIMER': 0,
         'PRIMER_PICK_ANYWAY': 1,
         'PRIMER_INTERNAL_MAX_SELF_END': 8,
-        'PRIMER_MIN_SIZE': 18,
+        'PRIMER_MIN_SIZE': 15,
         'PRIMER_MAX_SIZE': 26,
         'PRIMER_OPT_TM': 60.0,
         'PRIMER_MIN_TM': 55.0,
@@ -52,29 +67,92 @@ leftflank = primer3.bindings.designPrimers(
         'PRIMER_PAIR_MAX_COMPL_END': 8,
     })
 
-if args.output:
-     args.output.write('LEFT PRIMER PAIR\n************************\n\n')
-     for k in sorted(leftflank.keys()):
-          args.output.write('%s\t%s\n' % (k,leftflank[k]))
-     args.output.write('\n\n')
-else:
-     print('LEFT PRIMER PAIR\n************************\n\n')
-     for k in sorted(leftflank.keys()):
-          print('%s\t%s' % (k,leftflank[k]))
-     print('\n\n')
-
-rightflank = primer3.bindings.designPrimers(
+lflankrprimer = primer3.bindings.designPrimers(
     {
-        'SEQUENCE_ID': 'Right Flank',
+        'SEQUENCE_ID': 'Left Flank',
         'SEQUENCE_TEMPLATE': seqrec,
-        'SEQUENCE_INCLUDED_REGION': [args.rightbound, args.rightflanksize]
+        'SEQUENCE_INCLUDED_REGION': [args.leftbound - args.leftflanksize, args.leftflanksize],
     },
     {
         'PRIMER_OPT_SIZE': 20,
         'PRIMER_TASK': 'pick_cloning_primers',
+        'PRIMER_PICK_LEFT_PRIMER': 0,
+        'PRIMER_PICK_RIGHT_PRIMER': 1,
         'PRIMER_PICK_ANYWAY': 1,
         'PRIMER_INTERNAL_MAX_SELF_END': 8,
-        'PRIMER_MIN_SIZE': 18,
+        'PRIMER_MIN_SIZE': 15,
+        'PRIMER_MAX_SIZE': 26,
+        'PRIMER_OPT_TM': 60.0,
+        'PRIMER_MIN_TM': 55.0,
+        'PRIMER_MAX_TM': 65.0,
+        'PRIMER_MIN_GC': 20.0,
+        'PRIMER_MAX_GC': 80.0,
+        'PRIMER_MAX_POLY_X': 100,
+        'PRIMER_NUM_RETURN': 1,
+        'PRIMER_INTERNAL_MAX_POLY_X': 100,
+        'PRIMER_SALT_MONOVALENT': 50.0,
+        'PRIMER_DNA_CONC': 50.0,
+        'PRIMER_MAX_NS_ACCEPTED': 5,
+        'PRIMER_MAX_SELF_ANY': 12,
+        'PRIMER_MAX_SELF_END': 8,
+        'PRIMER_PAIR_MAX_COMPL_ANY': 12,
+        'PRIMER_PAIR_MAX_COMPL_END': 8,
+    })
+
+
+if args.output:
+     args.output.write('LEFT FLANK PRIMERS\n***********************\n')
+     write_primers(lflanklprimer, lflankrprimer, args.output)
+else:
+     print('LEFT FLANK PRIMERS\n***********************\n')
+     print_primers(lflanklprimer, lflankrprimer)
+
+rflanklprimer = primer3.bindings.designPrimers(
+    {
+        'SEQUENCE_ID': 'Right Flank',
+        'SEQUENCE_TEMPLATE': seqrec,
+        'SEQUENCE_INCLUDED_REGION': [args.rightbound, args.rightflanksize],
+    },
+    {
+        'PRIMER_OPT_SIZE': 20,
+        'PRIMER_TASK': 'pick_cloning_primers',
+        'PRIMER_PICK_LEFT_PRIMER': 1,
+        'PRIMER_PICK_RIGHT_PRIMER': 0,
+        'PRIMER_PICK_ANYWAY': 1,
+        'PRIMER_INTERNAL_MAX_SELF_END': 8,
+        'PRIMER_MIN_SIZE': 15,
+        'PRIMER_MAX_SIZE': 26,
+        'PRIMER_OPT_TM': 60.0,
+        'PRIMER_MIN_TM': 55.0,
+        'PRIMER_MAX_TM': 65.0,
+        'PRIMER_MIN_GC': 20.0,
+        'PRIMER_MAX_GC': 80.0,
+        'PRIMER_MAX_POLY_X': 100,
+        'PRIMER_NUM_RETURN': 1,
+        'PRIMER_INTERNAL_MAX_POLY_X': 100,
+        'PRIMER_SALT_MONOVALENT': 50.0,
+        'PRIMER_DNA_CONC': 50.0,
+        'PRIMER_MAX_NS_ACCEPTED': 5,
+        'PRIMER_MAX_SELF_ANY': 12,
+        'PRIMER_MAX_SELF_END': 8,
+        'PRIMER_PAIR_MAX_COMPL_ANY': 12,
+        'PRIMER_PAIR_MAX_COMPL_END': 8,
+    })
+
+rflankrprimer = primer3.bindings.designPrimers(
+    {
+        'SEQUENCE_ID': 'Right Flank',
+        'SEQUENCE_TEMPLATE': seqrec,
+        'SEQUENCE_INCLUDED_REGION': [args.rightbound, args.rightflanksize],
+    },
+    {
+        'PRIMER_OPT_SIZE': 20,
+        'PRIMER_TASK': 'generic',
+        'PRIMER_PICK_LEFT_PRIMER': 0,
+        'PRIMER_PICK_RIGHT_PRIMER': 1,
+        'PRIMER_PICK_ANYWAY': 1,
+        'PRIMER_INTERNAL_MAX_SELF_END': 8,
+        'PRIMER_MIN_SIZE': 15,
         'PRIMER_MAX_SIZE': 26,
         'PRIMER_OPT_TM': 60.0,
         'PRIMER_MIN_TM': 55.0,
@@ -94,12 +172,10 @@ rightflank = primer3.bindings.designPrimers(
     })
     
 if args.output:
-     args.output.write('RIGHT PRIMER PAIR\n************************\n\n')
-     for k in sorted(rightflank.keys()):
-          args.output.write('%s\t%s\n' % (k,rightflank[k]))
-     args.output.write('\n\n')
+     args.output.write('\n\nRIGHT FLANK PRIMERS\n***********************\n')
+     write_primers(lflanklprimer, lflankrprimer, args.output)
 else:
-     print('RIGHT PRIMER PAIR\n************************\n\n')
-     for k in sorted(rightflank.keys()):
-          print('%s\t%s' % (k,rightflank[k]))
-     print('\n\n')
+     print('\n\nRIGHT FLANK PRIMERS\n***********************\n')
+     print_primers(lflanklprimer, lflankrprimer)
+     
+
